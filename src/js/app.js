72db5078f5d0b00ae582b8596443f320d69f01e6
VolodymyrKladochnyi <vladimir@coderiver.com.ua>
2018-04-19 17:33:43 +0300

    add Gallery

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,32 @@
 import sayHello from './lib/sayHello.js';
 import { Calculator } from './lib/Calc.js';
+import Gallery from './lib/Gallery';
+import 'slick-carousel';
 sayHello();
 
 let calc = new Calculator('.calc');
 
 let main = new Calculator('.main');
+
+
+var mainImgArr = [];
+var obj = {};
+$.ajax({
+  url: 'https://jsonplaceholder.typicode.com/photos?_limit=10',
+  type: 'POST',
+  dataType: 'jsonp',
+  jsonp: 'callback',
+  jsonpCallback: 'jsonpCallback',
+  success: function(result) {
+    $.each(result, function(i) {
+      var obj = {};
+      obj.src = result[i].url;
+      obj.thumb = result[i].thumbnailUrl;
+      mainImgArr.push(obj);
+    });
+  }});
+
+
+let gallery = new Gallery('.gallery');
+
+gallery.initSlick(mainImgArr);
